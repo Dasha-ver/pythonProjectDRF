6fe73394425ccb81a9f13be4e09f4b4d94de6442
Daria <53658673+Dasha-ver@users.noreply.github.com>
2024-03-06 21:43:56 +0300

    Delete myProject/belCitiesInfo directory

--- myProject/belCitiesInfo/bel_cities_info/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BelCitiesInfoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bel_cities_info'
--- myProject/belCitiesInfo/bel_cities_info/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Cities(models.Model):
-    name = models.CharField(max_length=60)
-    region = models.CharField(max_length=60)
-    year_of_foundation = models.IntegerField()
-
-    def __str__(self):
-        return f'{self.name} : {self.region}, {self.year_of_foundation} year'
